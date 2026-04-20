eefaf204babdfa5430b5b31b5ba88f83d2122d32
Deepak Gudi <18bcar4007@jainuniversity.ac.in>
2026-04-20 10:26:13 +0530

    Delete middleware.ts

--- frontend/src/app/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from '@clerk/nextjs'
-
-export default authMiddleware({
-  publicRoutes: ['/sign-in', '/sign-up', '/api/members/onboard'],
-})
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
